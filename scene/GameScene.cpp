e83f411565350d10026d43ac779a2fc600c3f5ea
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2022-05-10 10:30:46 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,92 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include "AxisIndicator.h"
+#include"PrimitiveDrawer.h"
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() 
+{
+	delete model_; 
+	delete debugCamera_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	debugCamera_ = new DebugCamera(1600,900);
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+}
+
+void GameScene::Update() 
+{
+	debugCamera_->Update();
+}
+
+void GameScene::Draw() {
+
+	for (int i = -10; i <= 10; i++)
+	{
+		PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0.0f + (i * 5), 0.0f , -50.0f), Vector3(0.0f + (i * 5), 0.0f, 50.0f), Vector4(255, 255, 255, 255));
+		PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(-50.0f, 0.0f, 0.0f + (i * 5)), Vector3(50.0f, 0.0f, 0.0f + (i * 5)), Vector4(255, 255, 255, 255));
+	}
+
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
